e43866fccad9159668173cfefaff66a96ecd3f9c
Takeru Ohta <phjgt308@gmail.com>
2022-10-24 05:22:34 +0900

    Add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,53 @@
+//! An error handling library for portable unrecoverable errors.
+//!
+//! This crate provides,
+//!
+//! - [`Failure`] struct that represents an unrecoverable error with an error code, message and user-level backtrace
+//!   - Error code and message are optional
+//!   - Constituted with simple types ([`u32`], [`String`], and [`Vec`] of those)
+//!     - Portable across process and language boundaries
+//!     - Optional [`serde`] support ("serde" feature)
+//!   - Doesn't implement [`std::error::Error`] trait
+//! - [`OrFail`] trait
+//!   - Backtrace location is appended to [`Failure`] each time when calling [`OrFail::or_fail()`]
+//!   - [`bool`], [`Option<_>`] and [`Result<_, _>`](std::result::Result) implement [`OrFail`]
+//!
+//! # Examples
+//!
+//! ```
+//! use orfail::{OrFail, Result};
+//!
+//! fn check_non_zero(n: isize) -> Result<()> {
+//!     (n != 0).or_fail()?;
+//!     Ok(())
+//! }
+//!
+//! fn safe_div(x: isize, y: isize) -> Result<isize> {
+//!     check_non_zero(y).or_fail()?;
+//!     Ok(x / y)
+//! }
+//!
+//! // OK
+//! assert_eq!(safe_div(4, 2), Ok(2));
+//!
+//! // NG
+//! assert_eq!(safe_div(4, 0).err().map(|e| e.to_string()),
+//!            Some(
+//! r#"failed due to "expected `true` but got `false`"
+//!   at src/lib.rs:7
+//!   at src/lib.rs:12
+//! "#.to_owned()));
+//! ```
+
+#![warn(missing_docs)]
 use std::marker::PhantomData;
 
+/// This trait allows for returning an error code held by [`Failure`].
 pub trait ErrorCode {
+    /// Returns an error code representing this instance.
     fn error_code(&self) -> u32;
 
+    /// Takes an error code and returns a message describing the code if possible.
     #[allow(unused_variables)]
     fn default_message(code: u32) -> Option<String> {
         None
@@ -15,12 +60,15 @@ impl ErrorCode for u32 {
     }
 }
 
+/// A marker trait representing that `C` can be converted into the type implementing this trait.
 pub trait TakeOver<C: ErrorCode>: ErrorCode {}
 
 impl<C: ErrorCode> TakeOver<C> for u32 {}
 
+/// This crate specific [`Result`](std::result::Result) type.
 pub type Result<T, C = u32> = std::result::Result<T, Failure<C>>;
 
+/// [`Failure`] typically represents an unrecoverable error with an error code, message, and backtrace.
 #[derive(Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Failure<C = u32> {
@@ -28,18 +76,21 @@ pub struct Failure<C = u32> {
         feature = "serde",
         serde(default, skip_serializing_if = "Option::is_none")
     )]
+    /// Error code.
     pub code: Option<u32>,
 
     #[cfg_attr(
         feature = "serde",
         serde(default, skip_serializing_if = "Option::is_none")
     )]
+    /// Error message.
     pub message: Option<String>,
 
     #[cfg_attr(
         feature = "serde",
         serde(default, skip_serializing_if = "Vec::is_empty")
     )]
+    /// Backtrace.
     pub backtrace: Vec<Location>,
 
     #[cfg_attr(feature = "serde", serde(skip))]
@@ -47,31 +98,28 @@ pub struct Failure<C = u32> {
 }
 
 impl<C: ErrorCode> Failure<C> {
+    /// Makes a new [`Failure`] instance whose backtrace contains the current caller location.
     #[track_caller]
-    pub fn new(code: C, message: impl Into<String>) -> Self {
-        Self {
-            code: Some(code.error_code()),
-            message: Some(message.into()),
-            ..Default::default()
-        }
+    pub fn new() -> Self {
+        Self::default()
     }
 
-    #[track_caller]
-    pub fn with_code(code: C) -> Self {
-        let code = code.error_code();
-        Self {
-            code: Some(code),
-            message: C::default_message(code),
-            ..Default::default()
+    /// Updates the error code of this [`Failure`] instance.
+    ///
+    /// If the error message of this instance is `None` and `C::default_message(code.error_code())` returns `Some(_)`,
+    /// the default message is set to the [`Failure::message`](#structfield.message) field.
+    pub fn code(mut self, code: C) -> Self {
+        self.code = Some(code.error_code());
+        if self.message.is_none() {
+            self.message = C::default_message(code.error_code());
         }
+        self
     }
 
-    #[track_caller]
-    pub fn with_message(message: impl Into<String>) -> Self {
-        Self {
-            message: Some(message.into()),
-            ..Default::default()
-        }
+    /// Updates the error message of this [`Failure`] instance.
+    pub fn message(mut self, message: impl Into<String>) -> Self {
+        self.message = Some(message.into());
+        self
     }
 }
 
@@ -112,14 +160,19 @@ impl<C: ErrorCode> std::fmt::Display for Failure<C> {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// A location in the backtrace of a [`Failure`] instance.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Location {
+    /// File name.
     pub file: String,
+
+    /// Line number.
     pub line: u32,
 }
 
 impl Location {
+    /// Makes a new [`Location`] instance containing the caller's file name and line number.
     #[track_caller]
     pub fn new() -> Self {
         let location = std::panic::Location::caller();
@@ -130,13 +183,18 @@ impl Location {
     }
 }
 
+/// This trait allows for converting a value into `Result<_, Failure>`.
 pub trait OrFail<C: ErrorCode>: Sized {
+    /// Success value type (used for the `Ok(_)` variant).
     type Value;
+
+    /// Error value type (used for the `Err(_)` variant).
     type Error;
 
+    /// Returns `Err(Failure<C>)` if `self` is a failure value.
+    ///
+    /// If `Err(_)` is returned, this method should add the current caller location to the backtrace of the resulting `Failure<C>`.
     fn or_fail(self) -> Result<Self::Value, C>;
-
-    fn or_fail_with(self, f: impl FnOnce(Self::Error) -> Failure<C>) -> Result<Self::Value, C>;
 }
 
 impl<C: ErrorCode> OrFail<C> for bool {
@@ -145,15 +203,10 @@ impl<C: ErrorCode> OrFail<C> for bool {
 
     #[track_caller]
     fn or_fail(self) -> Result<Self::Value, C> {
-        self.or_fail_with(|()| Failure::with_message("expected `true` but got `false`"))
-    }
-
-    #[track_caller]
-    fn or_fail_with(self, f: impl FnOnce(Self::Error) -> Failure<C>) -> Result<Self::Value, C> {
         if self {
             Ok(())
         } else {
-            Err(f(()))
+            Err(Failure::new().message("expected `true` but got `false`"))
         }
     }
 }
@@ -164,15 +217,10 @@ impl<T, C: ErrorCode> OrFail<C> for Option<T> {
 
     #[track_caller]
     fn or_fail(self) -> Result<Self::Value, C> {
-        self.or_fail_with(|()| Failure::with_message("expected `Some(_)` but got `None`"))
-    }
-
-    #[track_caller]
-    fn or_fail_with(self, f: impl FnOnce(Self::Error) -> Failure<C>) -> Result<Self::Value, C> {
         if let Some(value) = self {
             Ok(value)
         } else {
-            Err(f(()))
+            Err(Failure::new().message("expected `Some(_)` but got `None`"))
         }
     }
 }
@@ -183,12 +231,7 @@ impl<T, E: std::error::Error, C: ErrorCode> OrFail<C> for std::result::Result<T,
 
     #[track_caller]
     fn or_fail(self) -> Result<Self::Value, C> {
-        self.map_err(|e| Failure::with_message(e.to_string()))
-    }
-
-    #[track_caller]
-    fn or_fail_with(self, f: impl FnOnce(Self::Error) -> Failure<C>) -> Result<Self::Value, C> {
-        self.map_err(f)
+        self.map_err(|e| Failure::new().message(e.to_string()))
     }
 }
 
@@ -214,14 +257,9 @@ where
             }
         }
     }
-
-    #[track_caller]
-    fn or_fail_with(self, f: impl FnOnce(Self::Error) -> Failure<D>) -> Result<Self::Value, D> {
-        self.or_fail().map_err(f)
-    }
 }
 
-/// Similiar to [`std::todo!()`] but returning an `Err(Failure)` instead of panicking.
+/// Similar to [`std::todo!()`] but returning an `Err(Failure)` instead of panicking.
 #[macro_export]
 macro_rules! todo {
     () => {
@@ -229,7 +267,7 @@ macro_rules! todo {
     };
 }
 
-/// Similiar to [`std::unreachable!()`] but returning an `Err(Failure)` instead of panicking.
+/// Similar to [`std::unreachable!()`] but returning an `Err(Failure)` instead of panicking.
 #[macro_export]
 macro_rules! unreachable {
     () => {
@@ -252,12 +290,13 @@ mod tests {
         assert_eq!(failure.backtrace.len(), 1);
 
         let failure: Failure = false
-            .or_fail_with(|_| Failure::with_code(10))
+            .or_fail()
+            .map_err(|f| f.code(10))
             .or_fail()
             .err()
             .unwrap();
         assert_eq!(failure.code, Some(10));
-        assert!(failure.message.is_none());
+        assert!(failure.message.is_some());
         assert_eq!(failure.backtrace.len(), 2);
     }
 }
